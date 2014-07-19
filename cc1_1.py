4f988c3a8017ac02db5b06755fbce50f86ff0105
billzhang1 <adam.zb@hotmail.com>
2014-07-19 00:34:30 -0400

    second way for 1_1

--- cc1_1.py
+++ cc1_1.py
@@ -1,5 +1,6 @@
 class Solution(object):
-    def uniquechar(self, string):
+# time complexity is O(nlog(n)), space comp is O(1)
+    def uniquechar1(self, string):
         if string == '':
             return True
 
@@ -14,6 +15,21 @@ class Solution(object):
 
         else:
             return True
+	
+    def uniquechar2(self, string):
+	boolean_list = []
+	for i in range(256):
+	    boolean_list.append(True)
+	for char in string:
+	    if(boolean_list[ord(char)]):
+		boolean_list[ord(char)] = False
+	    else:
+		return False
+	return True	
 
 s = Solution()
-print s.uniquechar("q")
+print s.uniquechar1("q")
+print s.uniquechar1("")
+print s.uniquechar1("qqqqaaasq")
+print s.uniquechar1("aldwocfq")
+print s.uniquechar2("qqqqqqqqqqqqqq")
